3d0d47d730960452682d0812406a061e8bb267c3
fouric <fouric@protonmail.com>
2019-08-31 20:46:02 -0700

    add do-nothing CLI script; cut out a bunch of code from stub main function

---
+++ cc.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/fish
+
+# https://interrupt.memfault.com/blog/building-a-cli-for-firmware-projects
+
+function cc
+    set scriptdir (dirname (status --current-filename))
+    # test for argument count and bail if none provided
+    if test (count $argv) -eq 0
+        echo "error: insufficient arguments" 1>&2
+        cc help
+        return 1
+    end
+
+    switch $argv[1]
+        case help
+            echo
+            echo "usage:"
+            echo "    help: display help/usage"
+            echo "    repo"
+            echo "    video"
+        case repo
+            firefox "https://github.com/fouric/cavern-chronicle"
+        case video
+            nohup mpv $HOME/ramdisk/Episode\ 1* 2>&1 >/dev/null &; disown
+        case '*'
+            echo "error: unrecognized option: " $argv[1] 1>&2
+            cc help
+    end
+end
+
+cc $argv
